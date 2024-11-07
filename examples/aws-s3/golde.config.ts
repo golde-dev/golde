2db9900aad9cdf7569b80171e0fcb0842e98ae29
chyzwar <schizek.marcin@gmail.com>
2024-11-07 21:57:14 +0100

    feat(): added s3 plan

--- examples/aws-s3/golde.config.ts
+++ examples/aws-s3/golde.config.ts
@@ -2,15 +2,24 @@ import type {Config} from '@golde/cli';
 
 const config: Config = {
   name: "example-aws-s3",
+  tags: {
+    Project: "GoldeExamples"
+  },
   providers: {
     golde: {
       apiKey: "{{ env.GOLDE_API_KEY }}",
     },
+    aws: {
+      accessKeyId: "{{ env.AWS_ACCESS_KEY_ID }}",
+      secretAccessKey: "{{ env.AWS_SECRET_ACCESS_KEY }}",
+    },
   },
   aws: {
     s3: {
-      "example-aws-s3": {
-        
+      "golde-example-aws-s3": {
+        tags: {
+          "BucketTag": "Example",
+        },
       }
     }
   }
