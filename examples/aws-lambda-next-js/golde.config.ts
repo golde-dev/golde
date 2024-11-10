6584fe9616614ddb302a1249fd7c89d8b641cf39
chyzwar <schizek.marcin@gmail.com>
2024-11-10 18:07:16 +0100

    chore(): added next.js example

---
+++ examples/aws-lambda-next-js/golde.config.ts
@@ -0,0 +1,28 @@
+import type {Config} from '@golde/cli';
+
+const config: Config = {
+  name: "example-aws-lambda-next-js",
+  tags: {
+    Project: "GoldeExamples"
+  },
+  providers: {
+    golde: {
+      apiKey: "{{ env.GOLDE_API_KEY }}",
+    },
+    aws: {
+      accessKeyId: "{{ env.AWS_ACCESS_KEY_ID }}",
+      secretAccessKey: "{{ env.AWS_SECRET_ACCESS_KEY }}",
+    },
+  },
+  aws: {
+    s3: {
+      "golde-example-aws-s3": {
+        tags: {
+          "BucketTag": "Example",
+        },
+      }
+    }
+  }
+};
+
+export default config
